11ac0ed41a79ad47a51697cbbc5743f60009dabb
fpellet <git@florentpellet.com>
2021-04-24 10:56:12 +0200

    Refactor tests

--- SupermarketReceipt.Test/SupermarketTest.cs
+++ SupermarketReceipt.Test/SupermarketTest.cs
@@ -10,38 +10,44 @@ namespace SupermarketReceipt.Test
 {
     public class SupermarketTest
     {
+        private readonly SupermarketCatalog _catalog;
+        private readonly Product _toothbrush;
+        private readonly Product _apples;
+
+        public SupermarketTest()
+        {
+            _catalog = new FakeCatalog();
+            _toothbrush = new Product("toothbrush", ProductUnit.Each);
+            _catalog.AddProduct(_toothbrush, 0.99);
+            _apples = new Product("apples", ProductUnit.Kilo);
+            _catalog.AddProduct(_apples, 1.99);
+        }
+
         [Fact]
         public void GoldenMasterTest()
         {
             List<double> dataSet = new List<double> { 2, 0, 1.5, 5 ,0.1, -1 };
             CombinationApprovals.VerifyAllCombinations((double applesQuantity, double toothbrushQuantity) =>
             {
-
-                // ARRANGE
-                SupermarketCatalog catalog = new FakeCatalog();
-                var toothbrush = new Product("toothbrush", ProductUnit.Each);
-                catalog.AddProduct(toothbrush, 0.99);
-                var apples = new Product("apples", ProductUnit.Kilo);
-                catalog.AddProduct(apples, 1.99);
-
-                var cart = new ShoppingCart();
-                if (applesQuantity != 0)
-                    cart.AddItemQuantity(apples, applesQuantity); 
-
-                if (toothbrushQuantity != 0)
-                cart.AddItemQuantity(toothbrush, toothbrushQuantity);
-
-                var teller = new Teller(catalog);
-                //teller.AddSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
-
-                // ACT
+                var cart = InitializeCart(applesQuantity, toothbrushQuantity);
+                var teller = new Teller(_catalog);
+                
                 var receipt = teller.ChecksOutArticlesFrom(cart);
 
-                // ASSERT
-                string result = new ReceiptPrinter().PrintReceipt(receipt);
-                //Approvals.Verify(result);
-                return result;
+                return new ReceiptPrinter().PrintReceipt(receipt);
             }, dataSet, dataSet);
+        }
+
+        private ShoppingCart InitializeCart(double applesQuantity, double toothbrushQuantity)
+        {
+            var cart = new ShoppingCart();
+            if (applesQuantity != 0)
+                cart.AddItemQuantity(_apples, applesQuantity);
+
+            if (toothbrushQuantity != 0)
+                cart.AddItemQuantity(_toothbrush, toothbrushQuantity);
+            
+            return cart;
         }
 
         [Fact]
@@ -51,32 +57,14 @@ namespace SupermarketReceipt.Test
             List<double> argumentDataSet = new List<double> { 2, 0, 1.5, 5 ,0.1, -1 };
             CombinationApprovals.VerifyAllCombinations((double applesQuantity, double toothbrushQuantity, double argument) =>
             {
-
-                // ARRANGE
-                SupermarketCatalog catalog = new FakeCatalog();
-                var toothbrush = new Product("toothbrush", ProductUnit.Each);
-                catalog.AddProduct(toothbrush, 0.99);
-                var apples = new Product("apples", ProductUnit.Kilo);
-                catalog.AddProduct(apples, 1.99);
-
-                var cart = new ShoppingCart();
-                if (applesQuantity != 0)
-                    cart.AddItemQuantity(apples, applesQuantity); 
-
-                if (toothbrushQuantity != 0)
-                cart.AddItemQuantity(toothbrush, toothbrushQuantity);
-
-                var teller = new Teller(catalog);
-                teller.AddSpecialOffer(SpecialOfferType.ThreeForTwo, apples, argument);
-                teller.AddSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush, argument);
-
-                // ACT
+                var cart = InitializeCart(applesQuantity, toothbrushQuantity);
+                var teller = new Teller(_catalog);
+                teller.AddSpecialOffer(SpecialOfferType.ThreeForTwo, _apples, argument);
+                teller.AddSpecialOffer(SpecialOfferType.ThreeForTwo, _toothbrush, argument);
+                
                 var receipt = teller.ChecksOutArticlesFrom(cart);
 
-                // ASSERT
-                string result = new ReceiptPrinter().PrintReceipt(receipt);
-                //Approvals.Verify(result);
-                return result;
+                return new ReceiptPrinter().PrintReceipt(receipt);
             }, productDataSet, productDataSet,argumentDataSet);
         }
 
@@ -87,32 +75,14 @@ namespace SupermarketReceipt.Test
             List<double> argumentDataSet = new List<double> { 2, 0, 1.5, 5 ,0.1, -1 };
             CombinationApprovals.VerifyAllCombinations((double applesQuantity, double toothbrushQuantity, double argument) =>
             {
-
-                // ARRANGE
-                SupermarketCatalog catalog = new FakeCatalog();
-                var toothbrush = new Product("toothbrush", ProductUnit.Each);
-                catalog.AddProduct(toothbrush, 0.99);
-                var apples = new Product("apples", ProductUnit.Kilo);
-                catalog.AddProduct(apples, 1.99);
-
-                var cart = new ShoppingCart();
-                if (applesQuantity != 0)
-                    cart.AddItemQuantity(apples, applesQuantity); 
-
-                if (toothbrushQuantity != 0)
-                cart.AddItemQuantity(toothbrush, toothbrushQuantity);
-
-                var teller = new Teller(catalog);
-                teller.AddSpecialOffer(SpecialOfferType.TenPercentDiscount, apples, argument);
-                teller.AddSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, argument);
-
-                // ACT
+                var cart = InitializeCart(applesQuantity, toothbrushQuantity);
+                var teller = new Teller(_catalog);
+                teller.AddSpecialOffer(SpecialOfferType.TenPercentDiscount, _apples, argument);
+                teller.AddSpecialOffer(SpecialOfferType.TenPercentDiscount, _toothbrush, argument);
+                
                 var receipt = teller.ChecksOutArticlesFrom(cart);
 
-                // ASSERT
-                string result = new ReceiptPrinter().PrintReceipt(receipt);
-                //Approvals.Verify(result);
-                return result;
+                return new ReceiptPrinter().PrintReceipt(receipt);
             }, productDataSet, productDataSet,argumentDataSet);
         }
 
@@ -123,32 +93,14 @@ namespace SupermarketReceipt.Test
             List<double> argumentDataSet = new List<double> { 2, 0, 1.5, 5 ,0.1, -1 };
             CombinationApprovals.VerifyAllCombinations((double applesQuantity, double toothbrushQuantity, double argument) =>
             {
-
-                // ARRANGE
-                SupermarketCatalog catalog = new FakeCatalog();
-                var toothbrush = new Product("toothbrush", ProductUnit.Each);
-                catalog.AddProduct(toothbrush, 0.99);
-                var apples = new Product("apples", ProductUnit.Kilo);
-                catalog.AddProduct(apples, 1.99);
-
-                var cart = new ShoppingCart();
-                if (applesQuantity != 0)
-                    cart.AddItemQuantity(apples, applesQuantity); 
-
-                if (toothbrushQuantity != 0)
-                cart.AddItemQuantity(toothbrush, toothbrushQuantity);
-
-                var teller = new Teller(catalog);
-                teller.AddSpecialOffer(SpecialOfferType.TwoForAmount, apples, argument);
-                teller.AddSpecialOffer(SpecialOfferType.TwoForAmount, toothbrush, argument);
-
-                // ACT
+                var cart = InitializeCart(applesQuantity, toothbrushQuantity);
+                var teller = new Teller(_catalog);
+                teller.AddSpecialOffer(SpecialOfferType.TwoForAmount, _apples, argument);
+                teller.AddSpecialOffer(SpecialOfferType.TwoForAmount, _toothbrush, argument);
+                
                 var receipt = teller.ChecksOutArticlesFrom(cart);
 
-                // ASSERT
-                string result = new ReceiptPrinter().PrintReceipt(receipt);
-                //Approvals.Verify(result);
-                return result;
+                return new ReceiptPrinter().PrintReceipt(receipt);
             }, productDataSet, productDataSet,argumentDataSet);
         }
 
@@ -159,32 +111,14 @@ namespace SupermarketReceipt.Test
             List<double> argumentDataSet = new List<double> { 2, 0, 1.5, 5 ,0.1, -1 };
             CombinationApprovals.VerifyAllCombinations((double applesQuantity, double toothbrushQuantity, double argument) =>
             {
-
-                // ARRANGE
-                SupermarketCatalog catalog = new FakeCatalog();
-                var toothbrush = new Product("toothbrush", ProductUnit.Each);
-                catalog.AddProduct(toothbrush, 0.99);
-                var apples = new Product("apples", ProductUnit.Kilo);
-                catalog.AddProduct(apples, 1.99);
-
-                var cart = new ShoppingCart();
-                if (applesQuantity != 0)
-                    cart.AddItemQuantity(apples, applesQuantity); 
-
-                if (toothbrushQuantity != 0)
-                cart.AddItemQuantity(toothbrush, toothbrushQuantity);
-
-                var teller = new Teller(catalog);
-                teller.AddSpecialOffer(SpecialOfferType.FiveForAmount, apples, argument);
-                teller.AddSpecialOffer(SpecialOfferType.FiveForAmount, toothbrush, argument);
-
-                // ACT
+                var cart = InitializeCart(applesQuantity, toothbrushQuantity);
+                var teller = new Teller(_catalog);
+                teller.AddSpecialOffer(SpecialOfferType.FiveForAmount, _apples, argument);
+                teller.AddSpecialOffer(SpecialOfferType.FiveForAmount, _toothbrush, argument);
+                
                 var receipt = teller.ChecksOutArticlesFrom(cart);
 
-                // ASSERT
-                string result = new ReceiptPrinter().PrintReceipt(receipt);
-                //Approvals.Verify(result);
-                return result;
+                return new ReceiptPrinter().PrintReceipt(receipt);
             }, productDataSet, productDataSet,argumentDataSet);
         }
     }
